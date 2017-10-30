83edd6871969ad835976e3736372c33c974aea78
Jeriel Jan del Prado <jerieljan@gmail.com>
2017-10-30 10:49:57 +0800

    Added Spring initializr results.

---
+++ src/main/java/hk/com/novare/hellobb/HelloBbApplication.java
@@ -0,0 +1,12 @@
+package hk.com.novare.hellobb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBbApplication.class, args);
+	}
+}
---
+++ src/test/java/hk/com/novare/hellobb/HelloBbApplicationTests.java
@@ -0,0 +1,16 @@
+package hk.com.novare.hellobb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloBbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
