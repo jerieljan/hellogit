e818dba10af9194e9f7cdfaac10f8e17c181c688
maryc <marycaraguinaldo@yahoo.com>
2017-11-28 16:36:38 +0800

    New user field has been added for more detailed information.

--- src/main/java/hk/com/novare/hellogit/controllers/AnnounceController.java
+++ src/main/java/hk/com/novare/hellogit/controllers/AnnounceController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jerieljan
  */
 @RestController
-@RequestMapping("announce")
+@RequestMapping(value="/announce/{name}")
 public class AnnounceController {
 
 
