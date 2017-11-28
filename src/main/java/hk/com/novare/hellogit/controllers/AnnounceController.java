839c832dc30c8f4f879e86e6ce3640999e1dfcae
maryc <marycaraguinaldo@yahoo.com>
2017-11-28 16:58:37 +0800

    New user field has been added for more detailed information.

--- src/main/java/hk/com/novare/hellogit/controllers/AnnounceController.java
+++ src/main/java/hk/com/novare/hellogit/controllers/AnnounceController.java
@@ -32,7 +32,7 @@ public class AnnounceController {
      * @return
      */
 
-    GetMapping({"", "/"})
+    @GetMapping({"", "/"})
     public ResponseEntity<UserDao> announceAll() {
         UserDao userDao = new UserDao();
         userDao.setStatus("Success.");
